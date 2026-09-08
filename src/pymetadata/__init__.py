407072983403cd576dae4cc3d26e42712e573d01
Matthias Koenig <konigmatt@googlemail.com>
2026-09-08 21:42:21 +0200

    Bump version: 0.6.1 → 0.6.2

--- src/pymetadata/__init__.py
+++ src/pymetadata/__init__.py
@@ -7,7 +7,7 @@ from pathlib import Path
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __author__ = "Matthias Koenig"
-__version__ = "0.6.1"
+__version__ = "0.6.2"
 
 
 program_name: str = "pymetadata"
